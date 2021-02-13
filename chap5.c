36b9696c8ce0de15397eaba9f76d06bd239f0866
s2ul2 <78787925+s2ul2@users.noreply.github.com>
2021-02-14 01:36:49 +0900

    chap5

---
+++ chap5.c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+/* 5-1, 1번
+int main()
+{
+	int left_x, left_y, right_x, right_y, result;
+
+	scanf("%d %d", &left_x, &left_y);
+	printf("좌 상단의 x, y 좌표 : %d %d\n", left_x, left_y);
+
+	scanf("%d %d", &right_x, &right_y);
+	printf("우 하단의 x, y 좌표 : %d %d\n", right_x, right_y);
+
+	result = (right_x - left_x) * (right_y - left_y);
+
+	printf("두 점이 이루는 직사각형의 넓이는 %d입니다.", result);
+
+
+} */
+/* 05-1, 4번
+int main()
+{
+	char num;
+
+	scanf("%d", &num); // 정수 형태로 데이터를 입력받는다.
+	printf("%c", num); // 데이터를 문자형태로 출력한다.
+
+} */
